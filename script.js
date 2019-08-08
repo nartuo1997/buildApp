64ba5663a0071fbaa76b94279004b580f8397483
root <root@DESKTOP-KJL66IM.localdomain>
2019-08-08 16:04:29 -0500

    upload files

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+var room = ['h1','h2','h3'];
+var newRoom = room.map(function (rm){
+    if (rm == 'h3') 
+    {
+        return 'h4'
+    }
+    else {
+        return rm
+    }
+
+})
+    console.log(room);
+    console.log(newRoom);
 
